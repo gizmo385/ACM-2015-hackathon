b20c6f15ab356bb994650f33a1f1d241262dd697
Christopher(gizmo385) <cachapline8@gmail.com>
2015-04-11 00:20:00 -0700

    Added some methods to the controller

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -7,19 +7,32 @@ import model.Structure;
 
 public class Controller {
 
-    public String[] numArgs(String method) {
-        if (method.equals("add")) {
-            return new String[] {"String", "Node"};
-        } else if (method.equals("delete")) {
-            return new String[] {"Node"};
-        } else {
-            return new String[] {};
-        }
+    /**
+     * Returns the number of arguments that are required a method defined on some structure.
+     *
+     * @param structure The structure that the method should be defined on.
+     * @param method The method whose argument count you are attempting to retrieve
+     *
+     * @return The number of arguments required by a certain method defined on a structure
+     *
+     */
+    public static int numArgs(Structure structure, String method) {
+        return getArgs(structure, method).length;
     }
 
-    public String[] getArgs(Structure structure) {
+    /**
+     * Returns the argumentables that a particular method takes on a given structure.
+     *
+     * @param structure The structure that the method should be defined on.
+     * @param method The method whose argument types you are attempting to retrieve
+     *
+     * @return An array of Argumentables, which are the types of arguments that can be represented
+     * by this visualizer
+     */
+    public static Argumentable[] getArgs(Structure structure, String method) {
         Map<String, Map<String, Argumentable>> operations = structure.getOperations();
+        Map<String, Argumentable> operationArgs = operations.get(method);
 
-        return null;
+        return (Argumentable[]) operationArgs.values().toArray();
     }
 }
