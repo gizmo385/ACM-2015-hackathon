68fa41388570d1d9f4e68ac9e0257a3e3dadc8f8
Christopher(gizmo385) <cachapline8@gmail.com>
2015-04-11 03:08:44 -0700

    Drawing panel will call render on whatever Structure is maintained in the controller

--- src/main/java/view/DrawingPanel.java
+++ src/main/java/view/DrawingPanel.java
@@ -1,7 +1,13 @@
-import javax.swing.JPanel;
+package view;
 
+import javax.swing.JPanel;
+import javax.swing.BorderFactory;
+
+import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+
+import controller.Controller;
 
 public class DrawingPanel extends JPanel {
 
@@ -11,18 +17,18 @@ public class DrawingPanel extends JPanel {
         this.WIDTH = width;
         this.HEIGHT = height;
 
-        initComponents();
         initPanel();
-    }
-
-    private void initComponents() {
     }
 
     private void initPanel() {
         super.setSize(WIDTH, HEIGHT);
+        super.setBorder( BorderFactory.createLineBorder(Color.black, 5) );
     }
 
     protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
         Graphics2D g2 = (Graphics2D)g;
+
+        Controller.renderStructure(g2);
     }
 }
